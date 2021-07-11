9f62cb3a9a6d60bc59acc00fbcaa085fcca3a8a5
Sergey Lukin <sergey.lukin@granulate.io>
2021-07-11 04:26:20 +0300

    Install prettier

--- lib/Techradar.jsx
+++ lib/Techradar.jsx
@@ -1,10 +1,10 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/react';
-import { React, useState, useEffect, useRef } from 'react';
-import D3Component from './D3Component';
-import fetchedData from './data';
+import { jsx } from "@emotion/react";
+import { React, useState, useEffect, useRef } from "react";
+import D3Component from "./D3Component";
+import fetchedData from "./data";
 
-import './main.css';
+import "./main.css";
 
 let vis;
 
@@ -17,37 +17,45 @@ export default function Techradar() {
 
   useEffect(fetchData, []);
   useEffect(handleResizeEvent, []);
-  useEffect(initVis, [ data ]);
-  useEffect(updateVisOnResize, [ width, height ]);
+  useEffect(initVis, [data]);
+  useEffect(updateVisOnResize, [width, height]);
 
   function fetchData() {
-    const IDEAL_BLIP_WIDTH = 22
-    Promise.resolve().then(() => setData(fetchedData.points.map((point, i) => ({...point, id: i, width: IDEAL_BLIP_WIDTH}))));
+    const IDEAL_BLIP_WIDTH = 22;
+    Promise.resolve().then(() =>
+      setData(
+        fetchedData.points.map((point, i) => ({
+          ...point,
+          id: i,
+          width: IDEAL_BLIP_WIDTH,
+        }))
+      )
+    );
   }
 
   function handleResizeEvent() {
     let resizeTimer;
     const handleResize = () => {
       clearTimeout(resizeTimer);
-      resizeTimer = setTimeout(function() {
+      resizeTimer = setTimeout(function () {
         setWidth(window.innerWidth);
         setHeight(window.innerHeight);
       }, 300);
     };
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }
 
   function initVis() {
-    if(data && data.length) {
+    if (data && data.length) {
       const d3Props = {
         data,
         width,
         height,
-        onDatapointClick: setActive
+        onDatapointClick: setActive,
       };
       vis = new D3Component(refElement.current, d3Props);
     }
@@ -57,7 +65,5 @@ export default function Techradar() {
     vis && vis.resize(width, height);
   }
 
-  return (
-      <div ref={refElement} />
-  );
+  return <div ref={refElement} />;
 }
